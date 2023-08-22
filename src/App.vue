adb36770c9663aaa0c94484939e803dec11e60b6
Grigory Stukunov <gstukunov@devexperts.com>
2023-08-23 00:26:40 +0300

    swiper added, added bio, added greeting

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="app-wrapper">
+    <BioCard />
+  </div>
+</template>
+
+<script>
+import BioCard from "./widgets/BioCard/BioCard.widget";
+
+export default {
+  name: "App",
+  components: {
+    BioCard,
+  },
+};
+</script>
+
+<style>
+html,
+body {
+  font-family: Fira Code;
+  margin: 0;
+  background-color: black;
+  --main-color: #fff;
+  --important-text: orange;
+  font-size: 13px;
+}
+.app-wrapper {
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
